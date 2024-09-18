4c8971ab9453b2c6264819b69ddf6619da0c41ec
ArwenOC <arwenatormc@gmail.com>
2024-09-18 01:47:50 -0700

    Fixed drop-down, added content panels for projects

--- scripts.js
+++ scripts.js
@@ -1,8 +1,19 @@
+// Credit : w3schools
+// Link : https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
 function hamburger() {
+    // Collects navbar element into variable
     var x = document.getElementById("nav");
-    if (x.className === "navbar") {
+    var y = document.getElementById("hamburger");
+
+    // If the class name is navbar, adds responsive css elements
+    if (x.className == "navbar") {
       x.className += " responsive";
+      // Sets hamborgir image
+      y.src = "images/hamburger_open.png";
     } else {
+      // Sets the class name back to navbar (Case in which the navbar is already clicked)
       x.className = "navbar";
+      // Sets hamborgir image
+      y.src = "images/hamburger.png";
     }
-  }
+}
