75ab61621d8bea962d94bb42868379ecab919c65
ArwenOC <arwenatormc@gmail.com>
2024-09-21 02:01:02 -0700

    Added logo to top of page, added tools page, and image for home page

--- scripts.js
+++ scripts.js
@@ -6,17 +6,20 @@ function hamburger() {
     // Collects navbar element into variable
     var x = document.getElementById("nav");
     var y = document.getElementById("hamburger");
+    var z = document.getElementById("nav_icon");
 
     // If the class name is navbar, adds responsive css elements
     if (x.className == "navbar") {
       x.className += " responsive";
       // Sets hamborgir image
       y.src = "images/hamburger_open.png";
+      z.style.display = "none";
     } else {
       // Sets the class name back to navbar (Case in which the navbar is already clicked)
       x.className = "navbar";
       // Sets hamborgir image
       y.src = "images/hamburger.png";
+      z.style.display = "block";
     }
 }
 
